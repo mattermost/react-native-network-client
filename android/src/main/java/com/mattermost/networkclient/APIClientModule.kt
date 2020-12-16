d6a155c699761451a6fdbd69f45d0562bbde3073
Shaz Amjad <shaz.amjad@mattermost.com>
2020-12-16 17:51:22 +1100

    feat: parse config options

--- android/src/main/java/com/mattermost/networkclient/APIClientModule.kt
+++ android/src/main/java/com/mattermost/networkclient/APIClientModule.kt
@@ -4,7 +4,7 @@ import com.facebook.react.bridge.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
-import com.facebook.react.bridge.Arguments;
+import okio.IOException
 
 class APIClientModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -16,92 +16,112 @@ class APIClientModule(reactContext: ReactApplicationContext) : ReactContextBaseJ
     }
 
     @ReactMethod
-    fun createClientFor(baseUrl: String, config: ReadableMap, promise: Promise){
+    fun createClientFor(baseUrl: String, options: ReadableMap, promise: Promise) {
         try {
+            // Create the client and request builder
             sessionsClient[baseUrl] = OkHttpClient().newBuilder();
             sessions[baseUrl] = Request.Builder().url(baseUrl);
 
-            val followRedirect = config.getBoolean("followRedirect")
-            if(followRedirect) sessionsClient[baseUrl]!!.followRedirects(followRedirect)
+            // Attach client options
+            sessionsClient[baseUrl]?.parseOptions(options);
 
-            promise.resolve(sessions[baseUrl]);
-        } catch (err: Throwable){
+            // Return stringified client for success
+            promise.resolve(sessions[baseUrl].toString())
+        } catch (err: Throwable) {
             promise.reject(err)
         }
     }
 
     @ReactMethod
-    fun getClientHeadersFor(baseUrl: String, promise: Promise){
+    fun getClientHeadersFor(baseUrl: String, promise: Promise) {
         try {
-            val headersMap = Arguments.createMap()
-            sessions[baseUrl]!!.build().headers.forEach{(k,v) -> headersMap.putString(k, v)}
-            promise.resolve(headersMap)
+            promise.resolve(sessions[baseUrl]?.build()?.headers?.readableMap())
         } catch (error: Error) {
             promise.reject(error)
         }
     }
 
     @ReactMethod
-    fun addClientHeadersFor(baseUrl: String, headers: ReadableMap, promise: Promise){
-        for ((k,v) in headers.toHashMap()){
-            sessions[baseUrl]!!.addHeader(k,v as String);
+    fun addClientHeadersFor(baseUrl: String, headers: ReadableMap, promise: Promise) {
+        try {
+            sessions[baseUrl]?.addReadableMap(headers)
+            this.getClientHeadersFor(baseUrl, promise)
+        } catch (error: Error) {
+            promise.reject(error)
         }
-        val headersMap = Arguments.createMap()
-        sessions[baseUrl]!!.build().headers.forEach{(k,v) -> headersMap.putString(k, v)}
-        promise.resolve(headersMap)
     }
 
     @ReactMethod
-    fun invalidateClientFor(baseUrl: String, promise: Promise){
+    fun invalidateClientFor(baseUrl: String, promise: Promise) {
         try {
             sessions.remove(baseUrl);
             promise.resolve(sessions.keys);
-        } catch (err: Throwable){
+        } catch (err: Throwable) {
             promise.reject(err)
         }
     }
 
     @ReactMethod
-    fun get(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise){
-        val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").build();
-        client.newCall(request).execute().use{ response ->
-            response.handleViaPromise(promise)
+    fun get(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise) {
+        try {
+            val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").parseOptions(options).build();
+            sessionsClient[baseUrl]!!.build().newCall(request).execute().use { response ->
+                response.promiseResolution(promise)
+            }
+        } catch (e: IOException) {
+            promise.reject(e)
         }
     }
 
     @ReactMethod
-    fun post(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise){
-        val body = options.toString().toRequestBody();
-        val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").post(body).build();
-        client.newCall(request).execute().use{ response ->
-            response.handleViaPromise(promise)
+    fun post(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise) {
+        try {
+            val body = options.getMap("body").toString().toRequestBody();
+            val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").post(body).parseOptions(options).build();
+            sessionsClient[baseUrl]!!.build().newCall(request).execute().use { response ->
+                response.promiseResolution(promise)
+            }
+        } catch (e: IOException) {
+            promise.reject(e)
         }
     }
 
     @ReactMethod
-    fun put(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise){
-        val body = options.toString().toRequestBody();
-        val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").put(body).build();
-        client.newCall(request).execute().use{ response ->
-            response.handleViaPromise(promise)
+    fun put(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise) {
+        try {
+            val body = options.getMap("body").toString().toRequestBody();
+            val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").put(body).parseOptions(options).build();
+            sessionsClient[baseUrl]!!.build().newCall(request).execute().use { response ->
+                response.promiseResolution(promise)
+            }
+        } catch (e: IOException) {
+            promise.reject(e)
         }
     }
 
     @ReactMethod
-    fun patch(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise){
-        val body = options.toString().toRequestBody();
-        val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").patch(body).build();
-        client.newCall(request).execute().use{ response ->
-            response.handleViaPromise(promise)
+    fun patch(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise) {
+        try {
+            val body = options.getMap("body").toString().toRequestBody();
+            val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").patch(body).parseOptions(options).build();
+            sessionsClient[baseUrl]!!.build().newCall(request).execute().use { response ->
+                response.promiseResolution(promise)
+            }
+        } catch (e: IOException) {
+            promise.reject(e)
         }
     }
 
     @ReactMethod
-    fun delete(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise){
-        val body = options.toString().toRequestBody();
-        val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").delete(body).build();
-        client.newCall(request).execute().use{ response ->
-            response.handleViaPromise(promise)
+    fun delete(baseUrl: String, endpoint: String, options: ReadableMap, promise: Promise) {
+        try {
+            val body = options.getMap("body").toString().toRequestBody();
+            val request = sessions[baseUrl]!!.url("$baseUrl/$endpoint").delete(body).parseOptions(options).build();
+            sessionsClient[baseUrl]!!.build().newCall(request).execute().use { response ->
+                response.promiseResolution(promise)
+            }
+        } catch (e: IOException) {
+            promise.reject(e)
         }
     }
 }
--- android/src/main/java/com/mattermost/networkclient/Helper.kt
+++ android/src/main/java/com/mattermost/networkclient/Helper.kt
@@ -2,12 +2,21 @@ package com.mattermost.networkclient
 
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.bridge.WritableMap
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.Response
 
-fun Response.returnAsWriteableMap (): WritableMap {
+/**
+ * Parses the response data into the format expected by the App
+ *
+ * @return WriteableMap for passing back to App
+ */
+fun Response.parse(): WritableMap {
     val headers = Arguments.createMap();
-    this.headers.forEach{ k -> headers.putString(k.first, k.second) }
+    this.headers.forEach { k -> headers.putString(k.first, k.second) }
 
     val map = Arguments.createMap()
     map.putMap("headers", headers)
@@ -17,10 +26,65 @@ fun Response.returnAsWriteableMap (): WritableMap {
     return map;
 }
 
-fun Response.handleViaPromise(promise: Promise){
-    return if(this.isSuccessful){
-        promise.resolve(this.returnAsWriteableMap());
+/**
+ * Completes the response handling by calling a promise resolve / reject based on response type
+ *
+ * @param promise The promise to resolve/reject
+ */
+fun Response.promiseResolution(promise: Promise): Response {
+    if (this.isSuccessful) {
+        promise.resolve(this.parse());
     } else {
         promise.reject(this.code.toString(), this.returnAsWriteableMap())
     }
+    return this
+}
+
+/**
+ * Parses options passed in over the bridge for individual requests
+ *
+ * @param options ReadableMap of options from the App
+ */
+fun Request.Builder.parseOptions(options: ReadableMap): Request.Builder {
+    return this
+}
+
+/**
+ * Parses options passed in over the bridge for client sessions
+ *
+ * @params options ReadableMap of options from the App
+ */
+fun OkHttpClient.Builder.parseOptions(options: ReadableMap): OkHttpClient.Builder {
+    this.followRedirects(false);
+    this.followSslRedirects(false);
+
+    val followRedirect = options.getBoolean("followRedirects")
+    if (followRedirect != null) {
+        this.followRedirects(followRedirect)
+        this.followSslRedirects(followRedirect)
+    }
+    return this
+}
+
+/**
+ * Parses the header data into the format expected by the App
+ *
+ * @return WriteableMap for passing back to App
+ */
+fun Headers.readableMap(): ReadableMap {
+    val headersMap = Arguments.createMap()
+    this.forEach { (k, v) -> headersMap.putString(k, v) }
+    return headersMap
+}
+
+/**
+ * Adds the headers object passed in by the app to the request
+ *
+ * @options headers ReadableMap
+ */
+fun Request.Builder.addReadableMap(headers: ReadableMap): Request.Builder {
+    for ((k, v) in headers.toHashMap()) {
+        this.addHeader(k, v as String);
+    }
+    return this
 }
