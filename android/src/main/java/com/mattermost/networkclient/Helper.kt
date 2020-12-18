21d4d6e66cafe221456dff523676f18200dfb073
Shaz Amjad <shaz.amjad@me.com>
2020-12-18 13:37:53 +1100

    fix: tidy naming

---
+++ android/src/main/java/com/mattermost/networkclient/Helper.kt
@@ -0,0 +1,25 @@
+package com.mattermost.networkclient
+
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.WritableMap
+import okhttp3.Response
+
+fun Response.returnAsWriteableMap (): WritableMap {
+    val headers = Arguments.createMap();
+    this.headers.forEach{ k -> headers.putString(k.first, k.second) }
+
+    val map = Arguments.createMap()
+    map.putMap("headers", headers)
+    map.putString("data", this.body!!.string())
+    map.putInt("code", this.code)
+    return map;
+}
+
+fun Response.handleViaPromise(promise: Promise){
+    return if(this.isSuccessful){
+        promise.resolve(this.returnAsWriteableMap());
+    } else {
+        promise.reject(this.code.toString(), this.returnAsWriteableMap())
+    }
+}
