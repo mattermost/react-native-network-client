fa3ec6a4545c8225dc41f7ed5fae06863a1ba43e
Miguel Alatzar <this.migbot@gmail.com>
2020-12-21 13:32:52 -0700

    fix: use baseUrl instead of host for clients

--- ios/Session+NetworkClient.swift
+++ ios/Session+NetworkClient.swift
@@ -15,9 +15,15 @@ extension Session: Equatable {
     }
 }
 
+fileprivate var baseUrl_FILEPRIVATE : [ObjectIdentifier:URL] = [:]
 fileprivate var cancelRequestsOnUnauthorized_FILEPRIVATE : [ObjectIdentifier:Bool] = [:]
 fileprivate var bearerAuthTokenResponseHeader_FILEPRIVATE : [ObjectIdentifier:String] = [:]
 extension Session {
+    var baseUrl: URL {
+        get { return baseUrl_FILEPRIVATE[ObjectIdentifier(self)]!}
+        set { baseUrl_FILEPRIVATE[ObjectIdentifier(self)] = newValue}
+    }
+
     var cancelRequestsOnUnauthorized: Bool {
         get { return cancelRequestsOnUnauthorized_FILEPRIVATE[ObjectIdentifier(self)] ?? false }
         set { cancelRequestsOnUnauthorized_FILEPRIVATE[ObjectIdentifier(self)] = newValue }
