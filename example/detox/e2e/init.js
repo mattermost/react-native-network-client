defec824722833f0ad7afe1daf1aa91a764db4a5
Joseph Baylon <joseph.baylon@mattermost.com>
2021-02-08 11:28:00 -0800

    Imported testConfig.siteUrl in init

--- example/detox/e2e/init.js
+++ example/detox/e2e/init.js
@@ -2,6 +2,7 @@
 // See LICENSE.txt for license information.
 
 import jestexpect from 'expect';
+import {siteUrl} from '@support/test_config';
 
 const http = require('http');
 const mockserver = require('mockserver');
@@ -22,17 +23,16 @@ beforeAll(async () => {
 });
 
 function launchMockserver() {
-    const mockserverUrl = 'http://localhost:8080';
-    const mockserverRunningMessage = `Mockserver running at ${mockserverUrl}`;
+    const runningMessage = `Mockserver running at ${siteUrl}`;
 
     // Check if mockserver is running
-    http.get(mockserverUrl, (res) => {
+    http.get(siteUrl, (res) => {
         jestexpect(res.statusCode).toBe(200);
-        console.log(mockserverRunningMessage);
+        console.log(runningMessage);
     }).on('error', (e) => {
         // Launch mockserver if not running
         console.log('MOCKSERVER NOT RUNNING! Launching mockserver...');
         http.createServer(mockserver('../mocks')).listen(8080);
-        console.log(mockserverRunningMessage);
+        console.log(runningMessage);
     });
 }
