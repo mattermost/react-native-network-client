4c32e31be4f845d6d565cbc79cc89bf1667059b6
Shaz Amjad <shaz.amjad@me.com>
2021-03-10 11:54:46 +1100

    fix: various android bugs

--- example/src/utils/index.tsx
+++ example/src/utils/index.tsx
@@ -159,6 +159,23 @@ const createMockserverAPIClient = async (): Promise<APIClientItem | null> => {
     });
 };
 
+const createRequestBinAPIClient = async (): Promise<APIClientItem | null> => {
+    const name = "RequestBin API";
+    const baseUrl =
+        Platform.OS === "ios"
+            ? "http://localhost:8000/14dkaof1"
+            : "http://10.0.2.2:8000/14dkaof1";
+    const headers = {
+        "header-1-key": "header-1-value",
+        "header-2-key": "header-2-value",
+    };
+    const configuration = buildDefaultApiClientConfiguration(headers);
+
+    return createAPIClient(name, baseUrl, configuration, {
+        validateUrl: false,
+    });
+};
+
 const createWebSocketClient = async (
     name: string,
     url: string,
@@ -198,7 +215,7 @@ const createWebSocketClient = async (
 const createMattermostWebSocketClient = async (): Promise<WebSocketClientItem | null> => {
     const name = "Mattermost Web Socket";
     const host =
-        Platform.OS === "ios" ? "ws://192.168.0.14:8065" : "ws://10.0.2.2:8080";
+        Platform.OS === "ios" ? "ws://192.168.0.14:8065" : "ws://10.0.2.2:8065";
     const url = `${host}/api/v4/websocket`;
     const configuration: WebSocketClientConfiguration = {
         headers: {
@@ -218,6 +235,7 @@ export const createTestClients = async (): Promise<NetworkClientItem[]> => {
         await createJSONPlaceholderAPIClient(),
         await createMockserverAPIClient(),
         await createMattermostWebSocketClient(),
+        await createRequestBinAPIClient(),
     ].reduce((clients: NetworkClientItem[], client) => {
         if (client) {
             return [...clients, client];
