896b6d1004d8b481f67acdcced7cac90f92690b4
Shaz Amjad <shaz.amjad@me.com>
2021-03-19 19:43:48 +1100

    feat: bytesRead progress event

--- android/src/main/java/com/mattermost/networkclient/helpers/UploadProgressListener.kt
+++ android/src/main/java/com/mattermost/networkclient/helpers/UploadProgressListener.kt
@@ -9,22 +9,25 @@ import java.text.DecimalFormat
 
 interface ProgressListenerInterface {
     fun update(bytesRead: Double, contentLength: Double)
-    fun emitProgressEvent(progress: Double)
+    fun emitProgressEvent(progress: Double, bytesRead: Double)
 }
 
 class ProgressListener(private val reactContext: ReactContext, private val taskId: String) : ProgressListenerInterface {
     // 2-Decimal places, rounded up
     private val df = DecimalFormat("#.00", ).apply{ roundingMode = RoundingMode.UP }
 
-    override fun emitProgressEvent(progress: Double) {
+    override fun emitProgressEvent(progress: Double, bytesRead: Double) {
         val params = Arguments.createMap()
         params.putDouble("fractionCompleted", progress)
         params.putString("taskId", "$taskId")
+        params.putDouble("bytesRead", bytesRead)
         reactContext.getJSModule(RCTDeviceEventEmitter::class.java).emit(APIClientEvents.UPLOAD_PROGRESS.event, params)
     }
 
     override fun update(bytesRead: Double, contentLength: Double) {
+        var progress = 0.0;
         // Only emit if we can show progress against content length
-        if(contentLength > 0) emitProgressEvent(df.format(bytesRead / contentLength).toDouble())
+        if(contentLength > 0) progress = df.format(bytesRead / contentLength).toDouble()
+        emitProgressEvent(progress, bytesRead)
     }
 }
