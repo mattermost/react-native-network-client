09a82915f09c3538c83c92d182e06225d5ab2506
Daniel Espino García <larkox@gmail.com>
2023-07-11 15:11:19 +0200

    Add missing changes

--- src/APIClient/index.tsx
+++ src/APIClient/index.tsx
@@ -37,8 +37,8 @@ const DEFAULT_API_CLIENT_CONFIG: APIClientConfiguration = {
     sessionConfiguration: {
         allowsCellularAccess: true,
         waitsForConnectivity: false,
-        timeoutIntervalForRequest: 30,
-        timeoutIntervalForResource: 30,
+        timeoutIntervalForRequest: 60000,
+        timeoutIntervalForResource: 604800000, // 7 days
         httpMaximumConnectionsPerHost: 10,
         cancelRequestsOnUnauthorized: false,
     },
