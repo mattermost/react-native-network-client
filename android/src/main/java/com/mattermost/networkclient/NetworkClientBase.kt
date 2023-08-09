130caccf03e08fab2bfcef101edbd103504eb4e3
Elias Nahum <nahumhbl@gmail.com>
2023-08-09 12:47:41 -0400

    fix: add back config variable

--- android/src/main/java/com/mattermost/networkclient/NetworkClientBase.kt
+++ android/src/main/java/com/mattermost/networkclient/NetworkClientBase.kt
@@ -464,6 +464,7 @@ internal open class NetworkClientBase(private val baseUrl: HttpUrl? = null) {
         var readTimeout = TimeoutInterceptor.defaultReadTimeout
         var writeTimeout = TimeoutInterceptor.defaultWriteTimeout
 
+        val config = options.getMap("sessionConfiguration")!!
         if (config.hasKey("timeoutIntervalForRequest")) {
                 try {
                     config.getDouble("timeoutIntervalForRequest").toInt().also { readTimeout = it }
