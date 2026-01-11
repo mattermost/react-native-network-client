74335214c4149f1e8554aadb5d92a78feab829a6
Elias Nahum <nahumhbl@gmail.com>
2026-01-11 14:25:32 +0200

    fix(ios): new architecture support

--- ios/ApiClient/ApiClient.h
+++ ios/ApiClient/ApiClient.h
@@ -2,18 +2,19 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTEventEmitter.h>
 
-#if RCT_NEW_ARCH_ENABLED
+#if RCT_NEW_ARCH_ENABLED && __cplusplus
+// Only import C++ headers when compiling as Objective-C++
 #import <React/RCTConvert.h>
 #import <RCTTypeSafety/RCTConvertHelpers.h>
-
 #import <NetworkClientSpec/NetworkClientSpec.h>
 @interface ApiClient: RCTEventEmitter <NativeApiClientSpec>
-
+#elif RCT_NEW_ARCH_ENABLED
+// When compiling as Objective-C, just extend RCTEventEmitter
+// The protocol conformance will be checked when ApiClient.mm is compiled
+@interface ApiClient: RCTEventEmitter
 #else
-
 #import <React/RCTBridgeModule.h>
 @interface ApiClient : RCTEventEmitter <RCTBridgeModule>
-
 #endif
 
 @end
